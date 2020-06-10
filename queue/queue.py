d961ef330653bf69e296a885bbe3a98d244bd8ec
Babatunde <babatundea15@gmail.com>
2020-06-10 08:47:38 +0100

    created linked list for queue

--- queue/queue.py
+++ queue/queue.py
@@ -42,4 +42,60 @@ q.enqueue(4)
 q.enqueue(5)
 q.dequeue()
 print(q.storage)
+
+#With linked list
+class Queue:
+    def __init__(self, value=None, next_node=None):
+       #Here, we set the initial value of the node
+       self.value=value
+       self.next_node=next_node
+       
+#Define methods for getting values, getting next node, and setting the next node.
+
+    def get_values(self, value):
+        return self.value
+    
+    
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+# The linked list here
+
+
+class Linked_stack:
+    def __init__(self):
+        # the initial value will be a reference to head which is the first node in the list
+        self.head = None
+        # reference to tail of the list
+        #self.tail = None
+
+    def add_to_end(self, value):
+        # Regardless of if the list is empty or not, we will need a new node.
+        new_node = Queue(value)
+        # We need to think of two edge cases, whether the list is empty or not
+        # If the list is empty, we wrap the value in a node and make it the head
+        if not self.head:  # If there is no head, our new node becomes the head
+            self.head = new_node
+        else:  # if there is a head, our new node will be added to the last node on the list. To get to the last node on the list, we need to traverse it.
+            current_node = self.head
+            while current_node.get_next() is not None:
+                current = current_node.get_next()
+            # At the end of the list,
+            current.set_next(new_node)
+
+    def remove_from_start(self):
+        if not self.head:  # If there is no head
+            return None
+        else:  # Here, we are concerned about removing the first element which is the head element
+            current_node = self.head
+            while current_node.get_next() is not None:
+            # At the end of the list,
+            start_value = current_node.get_value()
+            #We remove the value of the last node
+            return start_value
+
+        
         
