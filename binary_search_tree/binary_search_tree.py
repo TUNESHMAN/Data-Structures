f610026a2457f136296084173c5cc9e3b33e85f2
Babatunde <babatundea15@gmail.com>
2020-06-11 12:25:48 +0100

    fixed the for each

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -84,7 +84,16 @@ class BSTNode:
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self)
+
+        # Left case
+
+        if self.left:
+            self.left.for_each(fn)
+
+        # Right case
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
